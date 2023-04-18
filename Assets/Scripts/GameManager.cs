16b8be2c1bebf63ddaf590cf2060d637d7d83960
Rim3rh <124402943+Rim3rh@users.noreply.github.com>
2023-04-18 22:22:13 +0200

    he cambiado cosaS

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static GameManager Instance { get; private set; }
+    public int _playerHealth, _playerHunger, _playerOxygen;
+
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Hay mas de un GameManager");
+        }
+    }
     void Start()
     {
-        
+        _playerHealth = 100;
+        _playerHunger = 100;
+        _playerOxygen = 100;
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    public Slider _foodSlider, _healthSlider, _oxygenSlider;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // _foodSlider.value = GameManager.Instance._playerHunger;
+    }
+}
