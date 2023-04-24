6b609f072f82b38763f1d76c615eb7b6eaf2b391
anadriauwu <131257420+anadriauwu@users.noreply.github.com>
2023-04-24 11:23:31 +0200

    uwu

--- Assets/Scripts/FadeInFadeOut.cs
+++ Assets/Scripts/FadeInFadeOut.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FadeInFadeOut : MonoBehaviour
 {
@@ -13,6 +14,28 @@ public class FadeInFadeOut : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            int sceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+
+            if(sceneBuildIndex == 0)
+            {
+                SceneManager.LoadScene(1);
+            }
+            else
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    IEnumerator ChangeToSceneB()
+    {
+        Animation.set
+    }
+
+    IEnumerator ChangeToSceneA()
+    {
+
     }
 }
